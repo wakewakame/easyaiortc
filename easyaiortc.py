9d1fd8e964c740af4b2b08b69b031abc779dbda0
wakewakame <0214.taiki@gmail.com>
2021-08-11 19:22:03 +0900

    コメントの追加

--- easyaiortc.py
+++ easyaiortc.py
@@ -23,21 +23,30 @@ from IPython.display import display, HTML
 class CustomVideoStreamTrack(VideoStreamTrack):
     def __init__(self, recv_queue, send_queue):
         super().__init__()
+        # 映像受信キュー
         self.recv_queue = recv_queue
+
+        # 映像送信キュー
         self.send_queue = send_queue
+
+        # 最後に送信した画像
         self.last_image = None
 
     def addTrack(self, track):
+        # 受信した映像を受け取るタスク
         async def recv_frame():
             while True:
                 img = (await track.recv()).to_ndarray(format="rgb24")
                 try:
+                    # 受信した映像をキューに追加
                     self.recv_queue.put_nowait(img)
                 except queue.Full:
                     pass
         asyncio.Task(recv_frame())
 
     async def recv(self):
+        # まだ映像送信キューから一度も画像を受け取っていない場合は
+        # キューに画像が追加されるまで待機する
         if self.last_image is None:
             while self.last_image is None:
                 try:
@@ -46,20 +55,28 @@ class CustomVideoStreamTrack(VideoStreamTrack):
                     await asyncio.sleep(0.1)
         else:
             try:
+                # 映像送信キューから画像を取得
                 self.last_image = self.send_queue.get_nowait()
             except queue.Empty:
                 pass
+
+        # 映像送信
         frame = VideoFrame.from_ndarray(self.last_image, format="rgb24")
         frame.pts, frame.time_base = await self.next_timestamp()
         return frame
 
 
 async def start_webrtc(pc, signaling, recv_queue, send_queue, result_pipe_child):
+    # 映像の送受信を処理するクラス
     custom_video_track = CustomVideoStreamTrack(recv_queue, send_queue)
+
+    # 受信する映像がある場合はトラックを追加
     @pc.on("track")
     def on_track(track):
         if track.kind == "video":
             custom_video_track.addTrack(track)
+
+    # 接続の開始
     params = await signaling.connect()
     if params["is_initiator"] == "true":
         pc.addTrack(custom_video_track)
@@ -71,6 +88,8 @@ async def start_webrtc(pc, signaling, recv_queue, send_queue, result_pipe_child)
         result_pipe_child.send(None)
         result_pipe_child.close()
         return
+
+    # 終了するまでループ
     while True:
         obj = await signaling.receive()
         if isinstance(obj, RTCSessionDescription):
@@ -86,6 +105,7 @@ async def start_webrtc(pc, signaling, recv_queue, send_queue, result_pipe_child)
 
 
 def multiprocess_main(recv_queue, send_queue, room_id, result_pipe_child):
+    # この関数は別プロセスで実行される
     signaling = ApprtcSignaling(room_id)
     pc = RTCPeerConnection()
     loop = asyncio.new_event_loop()
@@ -98,19 +118,33 @@ def multiprocess_main(recv_queue, send_queue, room_id, result_pipe_child):
 
 class EasyAppRTC:
     def __init__(self, room_id=None, preview=False, width=1280, height=720):
+        # AppRTCの部屋IDが未指定の場合はここで部屋IDを決める
         if room_id == None:
             room_id = "".join([random.choice("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-") for x in range(8)])
+
+        # 映像受信キュー
         self.__recv_queue = mp.Queue(maxsize=1)
+
+        # 映像送信キュー
         self.__send_queue = mp.Queue(maxsize=1)
+
+        # 作成されたAppRTCの部屋URLを受け取るPipe
         result_pipe_parent, result_pipe_child = mp.Pipe()
+
+        # multiprocessingのProcess上でaiortcを動かすことにより、CPythonのGILによる問題を回避する。
+        # これを行わない場合、処理時間を要する画像処理などを行うと映像の受信処理が追いつかなくなる。
         self.__process = mp.Process(target=multiprocess_main, args=(self.__recv_queue, self.__send_queue, room_id, result_pipe_child))
         self.__process.daemon = True
         self.__process.start()
+
+        # 作成されたAppRTCの部屋URLを取得
         self.__room_url = result_pipe_parent.recv()
         if self.__room_url is None:
             print("failed to create room")
             self.close()
             return
+
+        # AppRTCの画面を表示
         if preview:
             display(HTML(
                 '<iframe id="inlineFrameExample" title="Inline Frame Example" width="{}" height="{}" src="{}" allow="microphone; camera"></iframe>'
@@ -120,17 +154,22 @@ class EasyAppRTC:
     def __del__(self):
         self.close()
     
+    # 接続の終了
     def close(self):
         if self.__process.is_alive():
             self.__process.kill()
             self.__process.join()
     
+    # 接続状態の取得
     def is_alive(self):
         return self.__process.is_alive()
 
+    # AppRTCの部屋URLを取得
     def room_url(self):
         return self.__room_url
     
+    # AppRTCから受信した映像を取得
+    # 受信した映像が無ければNoneを返す
     def get(self):
         if not self.is_alive():
             return None
@@ -141,6 +180,7 @@ class EasyAppRTC:
             pass
         return img
     
+    # AppRTCに映像を送信する
     def put(self, img):
         if not self.is_alive():
             return
@@ -148,21 +188,3 @@ class EasyAppRTC:
             self.__send_queue.put_nowait(img)
         except queue.Full:
             pass
-
-
-if __name__ == "__main__":
-    rtc = EasyAppRTC()
-    print("room url is {}".format(rtc.room_url()))
-    deg = 0
-    try:
-        while rtc.is_alive():
-            img = rtc.get()
-            if img is None:
-                continue
-            n_h, n_w, n_ch = img.shape
-            M = cv2.getRotationMatrix2D((n_w / 2, n_h / 2), deg, 1)
-            img = cv2.warpAffine(img, M, (n_w, n_h))
-            deg += 1
-            rtc.put(img)
-    except KeyboardInterrupt:
-        pass
